f969c2ebe8707dd7804affe027f671088f82bdad
GAURAV GUNNAL <122206333+gauravgunnal@users.noreply.github.com>
2023-06-04 13:09:35 +0530

    Add files via upload

---
+++ ASSIGNMENT2.py
@@ -0,0 +1,95 @@
+'''Q1'''
+# This is a single-line comment
+print("Hello, World!")  # This is also a comment
+
+"""
+This is a multi-line comment.
+It can span across multiple lines.
+"""
+print("Hello, World!")
+
+'''Q2'''
+'''In Python, a variable is a named reference to a value stored in the computer's memory. 
+It acts as a container to hold data, allowing you to manipulate and access it throughout your program.
+To declare and assign a value to a variable in Python, you can follow these steps:
+Declaration: Variables in Python do not need explicit declaration before assigning a value. You can 
+directly start using a variable name to declare it. For example:'''
+
+name = "GAURAV"
+'''In this example, we declare a variable named name and assign the value "GAURAV" to it. Python recognizes the variable 
+name as a new identifier and allocates memory to store the value.'''
+
+'''Assignment: Assigning a value to a variable is done using the assignment operator =. The value on the right side of the = operator 
+is assigned to the variable on the left side. For example:'''
+age = 24
+'''In this case, we assign the value 24 to the variable age.'''
+
+'''Q3'''
+x = int(3.14)
+print(x)  # Output: 3
+
+y = float("2.718")
+print(y)  # Output: 2.718
+
+z = str(42)
+print(z)  # Output: "42"
+
+'''Q4'''
+'''To write and execute a Python script from the command line, follow these steps:
+1. Open a text editor and create a new file. Enter your Python code in this file.
+2. Save the file with a `.py` extension, which is the convention for Python script files.
+3. Open the command line or terminal on your computer.
+4. Navigate to the directory where you saved your Python script using the `cd` command. 
+For example, if your script is saved in the "Documents" folder, you can use the following command in the terminal:
+
+   ```shell
+   cd Documents
+   ```
+5. Once you are in the correct directory, you can execute the Python script using the `python` command followed by the name of your script file. For example:
+
+   ```shell
+   python script.py
+   ```
+6. Press Enter to run the command. The Python script will be executed, and any output or results will be displayed in the terminal.
+
+'''Q5'''
+my_list = [1, 2, 3, 4, 5]
+sub_list = my_list[1:3]
+print(sub_list)
+
+'''Q6'''
+'''In mathematics, a complex number is a number that consists of a real part and an imaginary part.
+It is expressed in the form a + bi, where a represents the real part, b represents the imaginary part,
+and i is the imaginary unit, defined as the square root of -1.
+In Python, complex numbers are represented using the complex data type. The imaginary part is denoted by the
+letter "j" instead of "i". The syntax to create a complex number in Python is a + bj, where a and b can be
+any real numbers. Here's an example:'''
+z = 3 + 2j
+
+'''Q7'''
+'''The variable name "age" is on the left side of the assignment operator (=), 
+and the value 25 is on the right side. Python automatically infers the data type based on the 
+assigned value, so in this case, the variable "age" would be of type integer.'''
+age = 25
+
+'''Q8'''
+price = 9.99
+print(type(price)) #TYPE OF VARIBLE IS FLOAT
+
+'''Q9'''
+name = "GAURAV GUNNAL"
+print(name)
+
+'''Q10'''
+string = "Hello, World!"
+substring = string[7:12]
+print(substring)  # Output: World
+
+'''Q11'''
+'''the variable is_student is declared and assigned the boolean value True. 
+This indicates that you are currently a student.'''
+is_student = True
+'''is_student variable would hold the boolean value False, indicating that 
+you are not currently a student.'''
+is_student = False
+
